a9cfe563523c846a3940d3473bcafc46792a29e9
JHAO2401git <junhao2401@gmail.com>
2026-08-31 13:35:12 +0800

    update instance id

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+zip -r room-booking.zip . -x "*.git*"
+aws s3 cp room-booking.zip s3://room-booking-bucket/room-booking.zip
+aws ssm send-command \
+  --instance-ids "i-0b112d64267279005" \
+  --document-name "AWS-RunShellScript" \
+  --parameters commands='["aws s3 cp s3://room-booking-bucket/room-booking.zip /tmp/room-booking.zip","cd /tmp","rm -rf room-booking","unzip -o room-booking.zip -d room-booking","sudo cp -r room-booking/* /var/www/html/","sudo chown -R apache:apache /var/www/html","sudo systemctl restart httpd"]'

9dba96e22ff59a13c28374054c75c0b2b6d917b6
JHAO2401git <junhao2401@gmail.com>
2026-09-04 15:51:33 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 zip -r room-booking.zip . -x "*.git*"
 aws s3 cp room-booking.zip s3://room-booking-bucket/room-booking.zip
 aws ssm send-command \
-  --instance-ids "i-0a46d78312d84a1ef" \
+  --instance-ids "i-092ac9096735c4f06" \
   --document-name "AWS-RunShellScript" \
   --parameters commands='["aws s3 cp s3://room-booking-bucket/room-booking.zip /tmp/room-booking.zip","cd /tmp","rm -rf room-booking","unzip -o room-booking.zip -d room-booking","sudo cp -r room-booking/* /var/www/html/","sudo chown -R apache:apache /var/www/html","sudo systemctl restart httpd"]'
